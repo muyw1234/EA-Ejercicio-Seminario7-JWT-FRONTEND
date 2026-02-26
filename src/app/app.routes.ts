0e753da781465cdf2aea36edc3bf768e34b3a419
Eloi04 <eloi.zamora@estudiantat.upc.edu>
2026-02-26 12:26:59 +0100

    Version1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PostList } from './posts/post-list/post-list';
+import { PostDetail } from './posts/post-detail/post-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PostList },
+  { path: 'posts/:id', component: PostDetail }
+];
